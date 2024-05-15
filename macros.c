79e9ac3b7e23e761301358f0ff0aeb414bfa5138
PlagueEvent <eu.laptev@gmail.com>
2024-05-15 22:59:33 +0300

    update code

--- macros.c
+++ macros.c
@@ -5,6 +5,9 @@
 //TODO  написать еще один define scan_d(для строк 55,57) и заменить эти строки на макрос
 #define print(a,b,c,d,e,f,n) printf("%s %s %s %d %d %d %d\n", a,b,c,d,e,f,n)
 #define scan_s(str) scanf("%s", str)
+#define scan_d(num) scanf("%d", num)
+
+
 
 struct student {
 	char surname[50];
@@ -49,10 +52,13 @@ void inputStudents(struct student students[], int count) {
       scan_s(students[i].surname);
 			printf("Enter the student's name: ");
 			//scanf("%s", students[i].name);
+      scan_s(students[i].name);
 			printf("Enter the student's middle name: ");
 			//scanf("%s", students[i].patronymic);
+      scan_s(students[i].patronymic);
 			printf("Enter the student's group: ");
-			scanf("%d", &students[i].group);
+//			scanf("%d", &students[i].group);
+      scan_d(&students[i].group);
 			printf("Enter the math score: ");
 			scanf("%d", &students[i].mathematics);
 			printf("Enter a physics grade: ");
@@ -70,90 +76,6 @@ int main() {
 	outputStudents(students, count);
 return 0;
 }
-
-
-
-
-
-
-#include <stdio.h>
-#include <stdbool.h>
-
-#define print(a,b,c,d,e,f,n) printf("%s %s %s %d %d %d %d\n", a,b,c,d,e,f,n)
-#define scan_s(str) scanf("%s", str)
-#define scan_d(num) scanf("%d", &num)
-
-struct student {
- char surname[50];
- char name[50];
- char patronymic[50];
- int group;
- int mathematics;
- int physics;
- int chemistry;
-};
-
-
-void outputStudents(struct student students[], int count) {
- for (int j = 0; j < count; j++){
-  if (students[j].chemistry>=3){
-   students[j].chemistry = students[j].chemistry -1;
-  }
- }
-
-
-
-
-for (int i = 0; i < count; i++) {
-//#printf("%s %s %s %d %d %d %d\n",
-    print(
-    students[i].surname, 
-    students[i].name,
-    students[i].patronymic,
-    students[i].group,
-    students[i].mathematics,
-    students[i].physics,
-    students[i].chemistry);
-   }  
-}
-
-
-
-void inputStudents(struct student students[], int count) {
-  for (int i = 0; i < count; i++) {
-   printf("Enter the student's last name: ");
-      //scanf("%s", students[i].surname);
-      scan_s(students[i].surname);
-   printf("Enter the student's name: ");
-   //scanf("%s", students[i].name);
-   printf("Enter the student's middle name: ");
-   //scanf("%s", students[i].patronymic);
-   printf("Enter the student's group: ");
-   scan_d(students[i].group);
-   scanf("%d", &students[i].group);
-   printf("Enter the math score: ");
-   scan_d(students[i].mathematics);
-   printf("Enter a physics grade: ");
-   scanf("%d", &students[i].physics);
-   printf("Enter a chemistry grade: ");
-   scanf("%d", &students[i].chemistry);
-  }
-}
-int main() {
- int count = 0;
- printf("Enter the number of students: ");
- scanf("%d", &count);
- struct student students[count];
- inputStudents(students, count);
- outputStudents(students, count);
-return 0;
-}
-
-
-
-
-
-
 
 
 
